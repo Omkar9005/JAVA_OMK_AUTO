0772eb09824083c0d395d7d1c5c686ea0143f784
Omkar9005 <omkarrachcha@gmail.com>
2025-06-05 23:33:22 +0530

    hello world program added in git repo

---
+++ src/ex_01_Java_Basic/OR_001_Hello_world.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class OR_001_Hello_world {
+    public static void main(String[] args) {
+        System.out.println("hello I am running java program");
+    }
+}
