9ec06270170fa3d3fbe2ce31fc7aafb6fbfa389c
Omkar9005 <omkarrachcha@gmail.com>
2025-06-05 23:37:39 +0530

    updated changes

--- src/ex_01_Java_Basic/OR_001_Hello_world.java
+++ src/ex_01_Java_Basic/OR_001_Hello_world.java
@@ -3,5 +3,7 @@ package ex_01_Java_Basic;
 public class OR_001_Hello_world {
     public static void main(String[] args) {
         System.out.println("hello I am running java program");
+        System.out.println("updated changes push 2nd time");
+
     }
 }
