b6051afdef2cd0c73a35401139236bd09518c76e
Omkar9005 <omkarrachcha@gmail.com>
2025-06-11 20:18:31 +0530

    new code push on 11/06/2025

---
+++ src/lab004_Variables.java
@@ -0,0 +1,11 @@
+public class lab004_Variables {
+
+    public static void main(String[] args) {
+        //data_type variable_name = variable value
+        byte b = 10;
+        b= 10+20;
+        System.out.println("variable value can be change"+ b);
+        int i=10;
+        char c='j';
+    }
+}
